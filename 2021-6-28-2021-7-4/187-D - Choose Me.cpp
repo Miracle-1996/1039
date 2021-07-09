a76707d74ae3bc2a91cdd23c434f563092e42119
Miracle- <wmzhuo1996@163.com>
2021-07-09 21:13:21 +0800

    Create 187-D - Choose Me.cpp

---
+++ 2021-6-28-2021-7-4/187-D - Choose Me.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define hash _hash
+#define endl '\n'
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define cal1(x) __builtin_popcount(x)
+using namespace std;
+using namespace atcoder;
+using ll = int64_t;
+using int128 = __int128_t;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pli = pair<ll, int>;
+
+const ld pi = M_PI;
+const int N = 2e5 + 10;
+string str; int n, m, a[N], b[N]; ll s[N];
+
+template <class T> inline T &chmin(T &x, const T &y){ return x = min(x, y); }
+template <class T> inline T &chmax(T &x, const T &y){ return x = max(x, y); }
+
+int solve(void)
+{
+	int res = 0;
+	ll x = 0 - accumulate(a + 1, a + n + 1, 0ll);
+	for (int k = 1; k <= n; ++k) s[k] = 2ll * a[k] + b[k];
+	sort(s + 1, s + n + 1, greater<ll>());
+	for (int k = 1; x <= 0; ++k) { x += s[k]; ++res; }
+	return res;
+}
+
+signed main(int argc, char** argv)
+{
+	#ifdef Miracle
+		auto start_clock = clock();
+		freopen("input.txt", "r", stdin);
+	//	freopen("output.txt", "w", stdout);
+	#endif
+	std::ios::sync_with_stdio(false); cin.tie(nullptr);
+	while (cin >> n) {
+		for (int k = 1; k <= n; ++k) cin >> a[k] >> b[k];
+		cout << solve() << endl;
+	}
+	#ifdef Miracle
+        auto end_clock = clock();
+        cout << end_clock - start_clock << "ms" << endl;
+    #endif
+	return 0;	
+} 
