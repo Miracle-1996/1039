a9142ec1af810cbc0a2e6408e4371b66d1b9399c
Miracle- <wmzhuo1996@163.com>
2021-07-09 21:56:10 +0800

    Create 193-C - Unexpressed.cpp

---
+++ 2021-6-21-2021-6-27/193-C - Unexpressed.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define hash _hash
+#define endl '\n'
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define cal1(x) __builtin_popcount(x)
+using namespace std;
+using namespace atcoder;
+using ll = int64_t;
+using int128 = __int128_t;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pli = pair<ll, int>;
+
+const ld pi = M_PI;
+const int N = 2e5 + 10;
+string str; ll n, m, a[N];
+
+template <class T> inline T &chmin(T &x, const T &y){ return x = min(x, y); }
+template <class T> inline T &chmax(T &x, const T &y){ return x = max(x, y); }
+
+ll solve(void)
+{
+	set<ll> s;
+	for (int a = 2; a <= n / a; ++a) {
+		ll x = 1ll * a * a;
+		while (x <= n) { s.insert(x); x *= a; }
+	}
+	return n - s.size();
+}
+
+signed main(int argc, char** argv)
+{
+	#ifdef Miracle
+		auto start_clock = clock();
+		freopen("input.txt", "r", stdin);
+	//	freopen("output.txt", "w", stdout);
+	#endif
+	std::ios::sync_with_stdio(false); cin.tie(nullptr);
+	while (cin >> n) {
+		cout << solve() << endl;
+	}
+	#ifdef Miracle
+        auto end_clock = clock();
+        cout << end_clock - start_clock << "ms" << endl;
+    #endif
+	return 0;	
+}
