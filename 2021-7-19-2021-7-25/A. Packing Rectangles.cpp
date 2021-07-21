9ddaddbca3f767781e30a309422c226cbf186b90
Miracle- <wmzhuo1996@163.com>
2021-07-21 16:49:09 +0800

    Create A. Packing Rectangles.cpp

---
+++ 2021-7-19-2021-7-25/A. Packing Rectangles.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define hash _hash
+#define endl '\n'
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define cal1(x) __builtin_popcount(x)
+using namespace std;
+using ll = int64_t;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pli = pair<ll, int>;
+
+const ld pi = acos(-1.0);
+const int N = 2e5 + 10;
+string str; int n, w, h, a[N];
+
+template <class T> inline T &chmin(T &x, const T &y){ return x = min(x, y); }
+template <class T> inline T &chmax(T &x, const T &y){ return x = max(x, y); }
+
+ll cal(ll x)
+{
+	ll val1 = x / w;
+	ll val2 = x / h;
+	return val1 * val2;
+}
+
+template <class tp> tp lower(tp l, tp r, const tp x)
+{
+	while (l + 1 < r) {
+		tp mid = l + (r - l) / 2;
+		(cal(mid) >= x) ? r = mid : l = mid;
+	}
+	return r;
+}
+
+ll solve(void)
+{
+	ll x = 1;
+	while (cal(x) < n) x <<= 1;
+	return lower(0ll, x, 1ll * n);;
+}
+
+signed main(int argc, char** argv)
+{
+	#ifdef Miracle
+		auto start_clock = clock();
+		freopen("input.txt", "r", stdin);
+	//	freopen("output.txt", "w", stdout);
+	#endif
+	std::ios::sync_with_stdio(false); cin.tie(nullptr);
+	while (cin >> w >> h >> n) {
+		cout << solve() << endl;
+	}
+	#ifdef Miracle
+        auto end_clock = clock();
+        cout << end_clock - start_clock << "ms" << endl;
+    #endif
+	return 0;	
+}
