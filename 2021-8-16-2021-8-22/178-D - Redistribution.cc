f6468dd28436300ee6ae954aa42c12e06ad5d7e8
Miracle- <wmzhuo1996@163.com>
2021-08-24 19:59:01 +0800

    Create 178-D - Redistribution.cc

---
+++ 2021-8-16-2021-8-22/178-D - Redistribution.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define hash _hash
+#define endl '\n'
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define cal1(x) __builtin_popcount(x)
+using namespace std;
+using namespace atcoder;
+using ll = int64_t;
+using int128 = __int128_t;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pli = pair<ll, int>;
+
+const ld pi = M_PI;
+const int N = 2e5 + 10;
+string str; int n, m, a[N];
+
+template <class T> inline T &chmin(T &x, const T &y){ return x = min(x, y); }
+template <class T> inline T &chmax(T &x, const T &y){ return x = max(x, y); }
+
+int solve(void)
+{
+	if (n <= 2) return 0;
+	const int mod = (int)1e9 + 7;
+	vector<int> f(n + 1, 0);
+	f[3] = 1;
+	for (int k = 3; k <= n; ++k) {
+		if (k != 3) f[k] = (f[k - 1] + f[k]) % mod;
+		if (k + 3 <= n) f[k + 3] = (f[k + 3] + f[k]) % mod;
+	}
+	return f[n];
+}
+
+signed main(int argc, char** argv)
+{
+	#ifdef Miracle
+		auto start_clock = clock();
+		freopen("input.txt", "r", stdin);
+	//	freopen("output.txt", "w", stdout);
+	#endif
+	cout << fixed << setprecision(20);
+	std::ios::sync_with_stdio(false); cin.tie(nullptr);
+	while (cin >> n) {
+		cout << solve() << endl;
+	}
+	#ifdef Miracle
+        auto end_clock = clock();
+        cout << end_clock - start_clock << "ms" << endl;
+    #endif
+	return 0;	
+}
